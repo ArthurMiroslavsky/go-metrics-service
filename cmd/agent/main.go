519da12556c0c4371678ae5d2f1422152c736df8
Arthur Miroslavsky <arthurmiroslavsky@gmail.com>
2022-04-24 13:16:15 +0500

    Add "signal.NotifyContext"

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"log"
 	"math/rand"
 	"net/http"
+	"os/signal"
 	"runtime"
+	"syscall"
 	"time"
 )
 
@@ -33,7 +36,7 @@ func sendRequest(client *http.Client, fn metricRequests) {
 		response, err := client.Do(value)
 
 		if err != nil {
-			log.Println("Error when sending the request", err)
+			log.Fatal("Error when sending the request", err)
 		}
 
 		defer response.Body.Close()
@@ -80,7 +83,7 @@ func counterMetricRequests(counters counterMap) mapOfRequests {
 	return m
 }
 
-func NewMonitor() {
+func NewMonitor(ctx context.Context) {
 	gauges := make(map[string]gauge)
 	counters := make(counterMap)
 
@@ -99,58 +102,72 @@ func NewMonitor() {
 		Transport: transport,
 	}
 
-	time.AfterFunc(0, func() {
-		for {
-			<-reportTimer.C
-			countersReqFn := func() mapOfRequests { return counterMetricRequests(counters) }
-			gaugesReqFn := func() mapOfRequests { return gaugeMetricRequests(gauges) }
-
-			sendRequest(client, countersReqFn)
-			sendRequest(client, gaugesReqFn)
-		}
-	})
-
 	for {
-		<-pollTimer.C
+		select {
+		case <-ctx.Done():
+			pollTimer.Stop()
+			reportTimer.Stop()
+			log.Println("shutting down server gracefully")
+			return
+		default:
+			time.AfterFunc(0, func() {
+				for {
+					<-reportTimer.C
+					countersReqFn := func() mapOfRequests { return counterMetricRequests(counters) }
+					gaugesReqFn := func() mapOfRequests { return gaugeMetricRequests(gauges) }
 
-		counter++
+					sendRequest(client, countersReqFn)
+					sendRequest(client, gaugesReqFn)
+				}
+			})
 
-		runtime.ReadMemStats(&rtm)
+			for {
+				<-pollTimer.C
 
-		gauges["Alloc"] = gauge(rtm.Alloc)
-		gauges["BuckHashSys"] = gauge(rtm.BuckHashSys)
-		gauges["Frees"] = gauge(rtm.Frees)
-		gauges["GCCPUFraction"] = gauge(rtm.GCCPUFraction)
-		gauges["GCSys"] = gauge(rtm.GCSys)
-		gauges["HeapAlloc"] = gauge(rtm.HeapAlloc)
-		gauges["HeapIdle"] = gauge(rtm.HeapIdle)
-		gauges["HeapInuse"] = gauge(rtm.HeapInuse)
-		gauges["HeapObjects"] = gauge(rtm.HeapObjects)
-		gauges["HeapReleased"] = gauge(rtm.HeapReleased)
-		gauges["HeapSys"] = gauge(rtm.HeapSys)
-		gauges["LastGC"] = gauge(rtm.LastGC)
-		gauges["Lookups"] = gauge(rtm.Lookups)
-		gauges["MCacheInuse"] = gauge(rtm.MCacheInuse)
-		gauges["MCacheSys"] = gauge(rtm.MCacheSys)
-		gauges["MSpanInuse"] = gauge(rtm.MSpanInuse)
-		gauges["MSpanSys"] = gauge(rtm.MSpanSys)
-		gauges["Mallocs"] = gauge(rtm.Mallocs)
-		gauges["NextGC"] = gauge(rtm.NextGC)
-		gauges["NumForcedGC"] = gauge(rtm.NumForcedGC)
-		gauges["NumGC"] = gauge(rtm.NumGC)
-		gauges["OtherSys"] = gauge(rtm.OtherSys)
-		gauges["PauseTotalNs"] = gauge(rtm.PauseTotalNs)
-		gauges["StackInuse"] = gauge(rtm.StackInuse)
-		gauges["StackSys"] = gauge(rtm.StackSys)
-		gauges["Sys"] = gauge(rtm.Sys)
-		gauges["TotalAlloc"] = gauge(rtm.TotalAlloc)
+				counter++
 
-		gauges["RandomValue"] = gauge(rand.Intn(1000000000000000))
+				runtime.ReadMemStats(&rtm)
 
-		counters["PollCount"] = counter
+				gauges["Alloc"] = gauge(rtm.Alloc)
+				gauges["BuckHashSys"] = gauge(rtm.BuckHashSys)
+				gauges["Frees"] = gauge(rtm.Frees)
+				gauges["GCCPUFraction"] = gauge(rtm.GCCPUFraction)
+				gauges["GCSys"] = gauge(rtm.GCSys)
+				gauges["HeapAlloc"] = gauge(rtm.HeapAlloc)
+				gauges["HeapIdle"] = gauge(rtm.HeapIdle)
+				gauges["HeapInuse"] = gauge(rtm.HeapInuse)
+				gauges["HeapObjects"] = gauge(rtm.HeapObjects)
+				gauges["HeapReleased"] = gauge(rtm.HeapReleased)
+				gauges["HeapSys"] = gauge(rtm.HeapSys)
+				gauges["LastGC"] = gauge(rtm.LastGC)
+				gauges["Lookups"] = gauge(rtm.Lookups)
+				gauges["MCacheInuse"] = gauge(rtm.MCacheInuse)
+				gauges["MCacheSys"] = gauge(rtm.MCacheSys)
+				gauges["MSpanInuse"] = gauge(rtm.MSpanInuse)
+				gauges["MSpanSys"] = gauge(rtm.MSpanSys)
+				gauges["Mallocs"] = gauge(rtm.Mallocs)
+				gauges["NextGC"] = gauge(rtm.NextGC)
+				gauges["NumForcedGC"] = gauge(rtm.NumForcedGC)
+				gauges["NumGC"] = gauge(rtm.NumGC)
+				gauges["OtherSys"] = gauge(rtm.OtherSys)
+				gauges["PauseTotalNs"] = gauge(rtm.PauseTotalNs)
+				gauges["StackInuse"] = gauge(rtm.StackInuse)
+				gauges["StackSys"] = gauge(rtm.StackSys)
+				gauges["Sys"] = gauge(rtm.Sys)
+				gauges["TotalAlloc"] = gauge(rtm.TotalAlloc)
+
+				gauges["RandomValue"] = gauge(rand.Intn(1000000000000000))
+
+				counters["PollCount"] = counter
+			}
+		}
 	}
+
 }
 
 func main() {
-	NewMonitor()
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM, syscall.SIGINT, syscall.SIGQUIT)
+	defer stop()
+
+	NewMonitor(ctx)
 }
